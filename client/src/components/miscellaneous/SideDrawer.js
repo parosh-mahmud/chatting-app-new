80bc889a5c09aac9964a842531b7e2dd87ad4fa5
parosh-mahmud <60932587+parosh-mahmud@users.noreply.github.com>
2023-09-19 16:27:22 +0600

    updated

--- client/src/components/miscellaneous/SideDrawer.js
+++ client/src/components/miscellaneous/SideDrawer.js
@@ -136,7 +136,7 @@ const SideDrawer = () => {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          MERN-CHAT-APP
+          CHAT-HUB
         </Text>
         <div>
           <Menu>
